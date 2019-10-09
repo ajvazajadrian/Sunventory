4066b83b2e6879ffcd9061713d0ada4a81ce262f
Adrian Ajvazi <avashavash@users.noreply.github.com>
2019-10-09 14:25:17 +0200

    layout is fixed for auth screens

--- code/public/javascripts/script.js
+++ code/public/javascripts/script.js
@@ -13,3 +13,13 @@ function closeNav() {
   document.getElementsByClassName("overlay")[0].style.width = "0%";
 }
 
+// function setbackground(){
+//   var index = Math.round(Math.random() * 5);
+//   var ColorValue = "#E6A9EC";
+//   if(index == 1) ColorValue = "#FFCCCC";
+//   if(index == 2) ColorValue = "#CCAFFF";
+//   if(index == 3) ColorValue = "#A6BEFF";
+//   if(index == 4) ColorValue = "#99FFFF";
+//   if(index == 5) ColorValue = "#D5CCBB";
+//   document.body.style.backgroundColor= ColorValue;
+//   }
